98464e6ee1a14dcacdcdda70737d9f6e0c42b7b8
Sangwoo, Kim <kimsw5861@naver.com>
2025-07-09 11:59:13 +0900

    20250709

---
+++ src/views/DataBinding.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>Hello, {{ msg }}</h1>
+  <h1 v-text="msg"></h1>
+  <h2 v-html="htmlStr"></h2>
+
+  <p><input type="text" v-model="valueModel" /></p>
+  <p><input type="number" v-model.number="numberModel" /></p>
+  <p>
+    <select v-model="city">
+      <option value="02">서울</option>
+      <option value="042">대전</option>
+      <option value="052">부산</option>
+    </select>
+  </p>
+
+  <p>
+    <lable>{{ checking }}</lable>
+    <input
+      type="checkbox"
+      true-value="yes"
+      false-value="no"
+      v-model="checking"
+    />
+  </p>
+
+  <p>{{ valueModel }}, {{ numberModel }}, {{ city }}</p>
+</template>
+
+<script>
+// view -- model 의 바인딩.
+export default {
+  data() {
+    return {
+      msg: "World!!!",
+      htmlStr: '<span style="color: red;">Red Text</span>',
+      valueModel: "홍길동",
+      numberModel: 10,
+      city: "042",
+      checking: "yes",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
---
+++ src/views/WatchView.vue
@@ -0,0 +1,31 @@
+<template>
+  <h3>Watch View</h3>
+  <textarea v-bind:disabled="char - full" v-model="messageContent"></textarea>
+  <p class="char-count">현재: {{ charCount }} 자 / 최대 50자</p>
+  <p>{{ feedMessage }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "",
+      messageContent: "",
+      charCount: 0,
+      charFull: false,
+      feedMessage: "메세지를 입력하세요",
+    };
+  },
+  watch: {
+    messageContent(nv, ov) {
+      console.log(nv + "," + ov);
+      this.charCount = nv.length;
+      // 50자 => charFull: true, feedMessage: '제한 50자입니다.'
+      if (nv.length >= 50) {
+        this.charFull = true;
+        this.feedMessage = "제한 50자입니다.";
+      }
+    },
+  },
+};
+</script>
